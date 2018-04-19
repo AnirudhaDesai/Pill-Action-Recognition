1d81648833d0832f0c29fc39694c931abdb428b4
AnirudhaDesai <desai.anirudhad@gmail.com>
2018-04-19 11:05:54 -0400

    user predict status

--- Code/server/prediction_service.py
+++ Code/server/prediction_service.py
@@ -10,11 +10,18 @@ sys.path.append(os.path.abspath('..'))
 from FeatureExtraction import extract_features
 import pickle
 import numpy as np
+from collections import defaultdict
 
 class PredictionService():
     m_twist, m_dispense, m_h2m, m_w2m = None, None, None, None
     db_session_factory = None
     helper = None
+    STATUS_YES = 'Y'
+    STATUS_NO = 'N'
+    
+    timeout_window = 300        # number of seconds after which predict status
+                                # times out
+    
     
     def __init__(self):
         pass
@@ -27,30 +34,59 @@ class PredictionService():
         PredictionService.m_dispense = pickle.load(open(path + 'm_dispense.pkl', 'rb'))
         PredictionService.m_h2m = pickle.load(open(path + 'm_h2m.pkl', 'rb'))
         PredictionService.m_w2m = pickle.load(open(path + 'm_w2m.pkl', 'rb'))
-    
+        PredictionService.user_predict_status = defaultdict()     # latest (status,time) of predicted "yes"
     @staticmethod
-    def predict(medicine_id, user_id, data):
-        data = np.array([data])
-        features = extract_features(data)
+    def predict(medicine_id, user_id, data, time):
+        # check if prediction to be done based on status. 
+        if not PredictionService.is_predicted(user_id, time):
+            data = np.array([data])
+            features = extract_features(data)
+            
+            p_twist = PredictionService.m_twist.predict(features)[0]
+            p_dispense = PredictionService.m_dispense.predict(features)[0]
+            p_h2m = PredictionService.m_h2m.predict(features)[0]
+            p_w2m = PredictionService.m_w2m.predict(features)[0]
+            
+            net_pred = p_twist + p_dispense + p_h2m + p_w2m
+            
+            if net_pred >= 2:
+                if p_twist == 1 or p_dispense == 1:
+                    cur_session = PredictionService.db_session_factory()
+                    installs = PredictionService.helper.get_all_installs(cur_session, 
+                                                                         u_id=user_id)
+                    meds = PredictionService.helper.get_all_medications(cur_session, 
+                                                                        m_id=medicine_id)[0]
+                    
+                    push_ids = []
+                    for install in installs:
+                        push_ids.append(install.push_id)
+                    
+                    PredictionService.helper.send_push_notification(push_ids, 
+                                                                    'Intake detected for - user: ' + user_id + ', medicine: ' + meds.m_name)
         
-        p_twist = PredictionService.m_twist.predict(features)[0]
-        p_dispense = PredictionService.m_dispense.predict(features)[0]
-        p_h2m = PredictionService.m_h2m.predict(features)[0]
-        p_w2m = PredictionService.m_w2m.predict(features)[0]
+    def is_predicted(u_id, time):
+        #check if the prediction is already made for the user 
+        # to be a 'Y' in the given window. If so, do not predict again.
+        predict_status = False
+        if u_id not in PredictionService.user_predict_status.keys():
+            # new user. No prediction made yet. return .
+            return predict_status
+        else:
+            u_status = PredictionService.user_predict_status[u_id]
         
-        net_pred = p_twist + p_dispense + p_h2m + p_w2m
-        
-        if net_pred >= 2:
-            if p_twist == 1 or p_dispense == 1:
-                cur_session = PredictionService.db_session_factory()
-                installs = PredictionService.helper.get_all_installs(cur_session, 
-                                                                     u_id=user_id)
-                meds = PredictionService.helper.get_all_medications(cur_session, 
-                                                                    m_id=medicine_id)[0]
-                
-                push_ids = []
-                for install in installs:
-                    push_ids.append(install.push_id)
-                PredictionService.helper.send_push_notification(push_ids, 
-                                                                'Intake detected for - user: ' + user_id + ', medicine: ' + meds.m_name)
+        if u_status[0] == PredictionService.STATUS_NO:
+            return predict_status
+        else:
+            # latest prediction was true. check if within the window
+            if time - u_status[1] > PredictionService.timeout_window:
+                return predict_status
+        predict_status = True
+        return predict_status
+
+    def change_status(u_id, time):
+        # set the status to "YES" after prediction        
+        if u_id not in PredictionService.user_predict_status.keys():
+            PredictionService.user_predict_status[u_id] = \
+                (PredictionService.STATUS_YES, time)
+    
         
